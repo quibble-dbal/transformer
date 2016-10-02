455988393535fb5d7888253debaada43d51e599b
Marijn Ophorst <marijn@sensimedia.nl>
2016-10-02 15:49:50 +0200

    test transformers

--- tests/TransformerTest.php
+++ tests/TransformerTest.php
@@ -3,6 +3,7 @@
 namespace Quibble\Tests;
 
 use Quibble\Transformer\Transformer;
+use stdClass;
 
 /**
  * Tests for transformers.
@@ -31,6 +32,37 @@ class TransformerTest
         yield assert($result['float'] === 1.2);
         yield assert($result['boolean'] === true);
         yield assert($result['string'] === '1');
+    }
+
+    /**
+     * We can also specify a return type {?} and cast to an object {?}.
+     */
+    public function testObjects(Transformer $transformer)
+    {
+        $result = ['foo' => ['bar' => 1]];
+        $result = $transformer->resource($result, function (stdClass $foo) : stdClass {});
+        yield assert($result instanceof stdClass);
+        yield assert($result->foo instanceof stdClass);
+    }
+
+    /**
+     * If actually callable, anything passed as a reference can be modified {?}
+     * {?} but if passed by value is left alone {?}.
+     */
+    public function testModification(Transformer $transformer)
+    {
+        $result = ['foo' => 1, 'bar' => ['baz' => 2], 'fizz' => 3];
+        $result = $transformer->resource(
+            $result,
+            function (int &$foo, stdClass &$bar, int $fizz) : stdClass {
+                $foo++;
+                $bar = (array) $bar;
+                $fizz++;
+            }
+        );
+        yield assert($result->foo === 2);
+        yield assert(gettype($result->bar) === 'array');
+        yield assert($result->fizz === 3);
     }
 }
 
