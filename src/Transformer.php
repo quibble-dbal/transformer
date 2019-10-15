e23e483a944618688e009bb6775a0e93c541227c
Marijn Ophorst <marijn@monomelodies.nl>
2019-10-15 14:55:26 +0200

    support objects natively too

--- src/Transformer.php
+++ src/Transformer.php
@@ -74,32 +74,43 @@ class Transformer
         }
 
         $collection = array_map(function ($resource) use ($transforms, $returnType) {
-            if ($this->stripNumericIndices) {
-                $resource = array_filter(
-                    $resource,
-                    function ($key) {
-                        return !is_numeric($key);
-                    },
-                    ARRAY_FILTER_USE_KEY
-                );
-            }
-            foreach ($transforms as $name => $transformers) {
-                if (!array_key_exists($name, $resource)) {
-                    continue;
+            if (is_array($resource)) {
+                if ($this->stripNumericIndices) {
+                    $resource = array_filter(
+                        $resource,
+                        function ($key) {
+                            return !is_numeric($key);
+                        },
+                        ARRAY_FILTER_USE_KEY
+                    );
                 }
-                foreach ($transformers as $transform) {
-                    $resource[$name] = $transform($resource[$name]);
+                foreach ($transforms as $name => $transformers) {
+                    if (!array_key_exists($name, $resource)) {
+                        continue;
+                    }
+                    foreach ($transformers as $transform) {
+                        $resource[$name] = $transform($resource[$name]);
+                    }
                 }
-            }
-            if (strtolower($returnType) == 'stdclass') {
-                $resource = (object)$resource;
-            } elseif (strtolower($returnType) == 'array') {
-                $resource = (array)$resource;
-            } elseif (class_exists($returnType)) {
-                $data = $resource;
-                $resource = new $returnType;
-                foreach ($data as $key => $value) {
-                    $resource->$key = $value;
+                if (strtolower($returnType) == 'stdclass') {
+                    $resource = (object)$resource;
+                } elseif (strtolower($returnType) == 'array') {
+                    $resource = (array)$resource;
+                } elseif (class_exists($returnType)) {
+                    $data = $resource;
+                    $resource = new $returnType;
+                    foreach ($data as $key => $value) {
+                        $resource->$key = $value;
+                    }
+                }
+            } elseif (is_object($resource)) {
+                foreach ($transforms as $name => $transformers) {
+                    if (!property_exists($resource, $name)) {
+                        continue;
+                    }
+                    foreach ($transformers as $transform) {
+                        $resource->$name = $transform($resource->$name);
+                    }
                 }
             }
             return $resource;
